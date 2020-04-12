2c754b21032c48678003b1ac50a5fefe9aa8302d
Mohmedhusain Vaid <hussainvaid7@gmail.com>
2020-04-12 00:08:49 -0500

    All the prompts are working as expected

--- Develop/app.js
+++ Develop/app.js
@@ -7,8 +7,55 @@ const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
 let empData;
+let managerData;
+let empList;
+let totalEmp = true;
 
+async function moreEmp() {
+  try {
+    while (totalEmp) {
+     await getInfo();
+
+      let confirm = await inquirer.prompt({
+        message: `Would you like to add more more members? (YES/NO): `,
+        name: `more`,
+      });
+      if (confirm.more.toUpperCase() == `NO`) {
+        totalEmp = false;
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//to get manager's info
+async function getManagerInfo() {
+  try {
+    console.log(`Enter the team managers information!`);
+    managerData = await inquirer.prompt([
+      {
+        type: "input",
+        message: "Enter name: ",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "Enter name ID: ",
+        name: "ID",
+      },
+    ]);
+  } catch (error) {
+    console.log(error);
+  }
+  managerData.title = "Manager";
+  console.log(managerData);
+  moreEmp();
+}
+
+//get employee info
 async function getInfo() {
+  console.log(`Enter the team members information`);
   try {
     empData = await inquirer.prompt([
       {
@@ -18,7 +65,7 @@ async function getInfo() {
       },
       {
         type: "input",
-        message: "Enter the employee role (Intern, Engineer or Manager): ",
+        message: "Enter the employee role (Intern or Engineer ): ",
         name: "role",
       },
       {
@@ -27,15 +74,16 @@ async function getInfo() {
         name: "ID",
       },
     ]);
+    empList= await getSpecial(empData);
   } catch (err) {
     console.log(err);
   }
-  getSpecial(empData);
+  //getSpecial(empData);
 }
 
-getInfo();
-
+// check the employee type in order to ask the next question
 async function getSpecial(empData) {
+try {
   switch (empData.role.toUpperCase()) {
     case "INTERN":
       empData.school = await inquirer.prompt({
@@ -49,10 +97,12 @@ async function getSpecial(empData) {
         name: "githib",
       });
       break;
-    case "MANAGER":
-      empData.office = await inquirer.prompt({
-        message: `Enter the office number: `,
-        name: "office",
-      });
   }
+  return empData;
+} catch (error) {
+  console.log(error);
 }
+  
+}
+
+getManagerInfo();
